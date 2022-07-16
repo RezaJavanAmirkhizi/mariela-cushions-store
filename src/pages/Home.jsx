0fe84f6da02f3483ddd55fd5e60eb4461fca7d4d
Reza Javan <javan.reza1380@gmail.com>
2022-07-16 12:20:16 +0430

    Add some defaults style and import style file

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,19 @@
 import Slider from "../components/home/Slider";
+import Description from "../components/home/Description";
+import AboutMe from "../components/home/AboutMe";
+import Details from "../components/home/Details";
+import NewestCollection from "../components/home/NewestCollection";
+import NewestCollectionProducts from "../components/home/NewestCollectionProducts";
 
 function Home() {
 	return (
 		<>
 			<Slider />
+			<Description />
+			<AboutMe />
+			<Details />
+			<NewestCollection />
+			<NewestCollectionProducts />
 		</>
 	);
 }
