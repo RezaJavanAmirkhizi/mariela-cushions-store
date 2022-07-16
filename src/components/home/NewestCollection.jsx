82a09c6710b44b7cd9a9a6fe764b90418ce75db2
Reza Javan <javan.reza1380@gmail.com>
2022-07-16 12:18:54 +0430

    Create newest collection banner and style for this

---
+++ src/components/home/NewestCollection.jsx
@@ -0,0 +1,17 @@
+function NewestCollection() {
+	return (
+		<div className="new-collection-con">
+			<div className="new-collection">
+				<div className="pic">
+				</div>
+				<div className="des">
+					<p>NEW</p>
+					<h1>Spring CushionCollection 2019</h1>
+					<a href="/">BUY NOW</a>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default NewestCollection;
