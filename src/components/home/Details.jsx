21695c7232e69dbdaa61929b9a14dc44a0e4746f
Reza Javan <javan.reza1380@gmail.com>
2022-07-16 12:18:05 +0430

    Create details page for home page and style for this

---
+++ src/components/home/Details.jsx
@@ -0,0 +1,83 @@
+function Details() {
+	return (
+		<div className="details">
+			<div className="detail">
+				<div className="context">
+					<p>{"beginnings".toUpperCase()}</p>
+					<span>
+						Credibly innovate granular internal or organic sources
+						whereas high standards in web-readiness. Energistically
+						scale future-proof core competencies vis-a-vis impactful
+						experiences.
+					</span>
+					<span>
+						Dramatically synthesize integrated schemas. with optimal
+						networks.
+					</span>
+				</div>
+				<div className="pic">
+					<img
+						alt="beginnings"
+						src="https://uploads-ssl.webflow.com/62be0f5753ff54221209cbb4/62be0f5853ff5416b009cbef_nathan-fertig-249917-unsplash.jpg"
+					/>
+				</div>
+			</div>
+			<div className="detail">
+				<div className="context">
+					<p>{"carrer".toUpperCase()}</p>
+					<span>
+						Collaboratively administrate turnkey channels whereas
+						virtual e-tailers.
+					</span>
+					<span>
+						Objectively seize scalable metrics whereas proactive
+						e-services seamlessly empower fully researched growth
+						strategies.
+					</span>
+					<span>
+						And interoperable internal or “organic” sources.
+					</span>
+					<div className="learn-more">
+						<a className="button" href="/">
+							LEARN MORE
+						</a>
+					</div>
+				</div>
+				<div className="pic">
+					<img
+						alt="beginnings"
+						src="https://uploads-ssl.webflow.com/62be0f5753ff54221209cbb4/62be0f5853ff5416b009cbef_nathan-fertig-249917-unsplash.jpg"
+					/>
+				</div>
+			</div>
+			<div className="detail">
+				<div className="context">
+					<p>{"beginnings".toUpperCase()}</p>
+					<span>
+						Credibly innovate granular internal or organic sources
+						whereas high standards in web-readiness. Energistically
+						scale future-proof core competencies vis-a-vis impactful
+						experiences.
+					</span>
+					<span>
+						Dramatically synthesize integrated schemas. with optimal
+						networks.
+					</span>
+					<div className="learn-more">
+						<a className="button" href="/">
+							LEARN MORE
+						</a>
+					</div>
+				</div>
+				<div className="pic">
+					<img
+						alt="beginnings"
+						src="https://uploads-ssl.webflow.com/62be0f5753ff54221209cbb4/62be0f5853ff5416b009cbef_nathan-fertig-249917-unsplash.jpg"
+					/>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default Details;
