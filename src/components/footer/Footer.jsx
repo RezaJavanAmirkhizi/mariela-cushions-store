4f6e90bf0200ba9c81c1fec0af781e71c666892a
Reza Javan <javan.reza1380@gmail.com>
2022-07-19 17:33:35 +0430

    Create footer for hom page and style for this

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,77 @@
+
+function Footer() {
+    return (
+		<div className="footer">
+			<div className="top">
+				<div className="item">
+					<div className="icon">
+						<img
+							alt="Safe shopping"
+							src="https://uploads-ssl.webflow.com/62be0f5753ff54221209cbb4/62be0f5853ff54bbb609cbdc_compras.svg"
+						/>
+					</div>
+					<span>Safe shopping</span>
+					<p>Buy with confidence</p>
+				</div>
+				<div className="item">
+					<div className="icon">
+						<img
+							alt="Fast shipping"
+							src="https://uploads-ssl.webflow.com/62be0f5753ff54221209cbb4/62be0f5853ff54c80409cbe0_entrega.svg"
+						/>
+					</div>
+					<span>Fast shipping</span>
+					<p>Get your product fast</p>
+				</div>
+				<div className="item">
+					<div className="icon">
+						<img
+							alt="Satisfaction guarantee"
+							src="https://uploads-ssl.webflow.com/62be0f5753ff54221209cbb4/62be0f5853ff54643609cbde_como.svg"
+						/>
+					</div>
+					<span>Satisfaction guarantee</span>
+					<p>Or get your money back</p>
+				</div>
+			</div>
+			<div className="bottom">
+				<div className="menu">
+					<a href="/" className="logo">
+						<img
+							alt="logo"
+							src="https://uploads-ssl.webflow.com/62be0f5753ff54221209cbb4/62be0f5853ff54041509cbe6_Logo-w-p-130x130q80.png"
+						/>
+					</a>
+					<nav>
+						<li>Home</li>
+						<li>About me</li>
+						<li>Products</li>
+						<li>Contact me</li>
+					</nav>
+				</div>
+				<div className="social-media">
+					<div className="media">
+						<img
+							alt="Facebook"
+							src="https://uploads-ssl.webflow.com/62be0f5753ff54221209cbb4/62be0f5853ff54cc5809cbe4_facebook-logo.svg"
+						/>
+					</div>
+					<div className="media">
+						<img
+							alt="Instagram"
+							src="https://uploads-ssl.webflow.com/62be0f5753ff54221209cbb4/62be0f5853ff547e7609cbdf_instagram%20(2).svg"
+						/>
+					</div>
+					<div className="media">
+						<img
+							alt="Twiter"
+							src="https://uploads-ssl.webflow.com/62be0f5753ff54221209cbb4/62be0f5853ff54f78309cbe3_gorjeo%20(1).svg"
+						/>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default Footer;
