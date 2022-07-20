ea81f21d6fbf5e02de5c8572cca483c7a74ca5cf
Reza Javan <javan.reza1380@gmail.com>
2022-07-20 13:21:35 +0430

    Add img tag

--- src/components/home/NewestCollection.jsx
+++ src/components/home/NewestCollection.jsx
@@ -3,6 +3,7 @@ function NewestCollection() {
 		<div className="new-collection-con">
 			<div className="new-collection">
 				<div className="pic">
+					<img alt="" src="https://imagescdn.simons.ca/images/8527/2221700/39/A1_2.jpg?__=2" />
 				</div>
 				<div className="des">
 					<p>NEW</p>
