f646d83e1e0056c524500f1aaf85a1f26b76de3d
Reza Javan <javan.reza1380@gmail.com>
2022-07-20 13:21:21 +0430

    Edit some part context

--- src/components/home/Details.jsx
+++ src/components/home/Details.jsx
@@ -45,23 +45,24 @@ function Details() {
 				</div>
 				<div className="pic">
 					<img
-						alt="beginnings"
-						src="https://uploads-ssl.webflow.com/62be0f5753ff54221209cbb4/62be0f5853ff5416b009cbef_nathan-fertig-249917-unsplash.jpg"
+						alt="carrer"
+						src="https://uploads-ssl.webflow.com/62be0f5753ff54221209cbb4/62be0f5853ff546f6b09cbf2_6.jpg"
 					/>
 				</div>
 			</div>
 			<div className="detail">
 				<div className="context">
-					<p>{"beginnings".toUpperCase()}</p>
+					<p>{"products".toUpperCase()}</p>
 					<span>
-						Credibly innovate granular internal or organic sources
-						whereas high standards in web-readiness. Energistically
-						scale future-proof core competencies vis-a-vis impactful
-						experiences.
+						Seamlessly empower fully researched growth strategies
+						and interoperable internal or “organic” sources.
 					</span>
 					<span>
-						Dramatically synthesize integrated schemas. with optimal
-						networks.
+						Credibly innovate granular internal or “organic” sources
+						whereas high standards in web-readiness.
+					</span>
+					<span>
+						Energistically scale future-proof core competence
 					</span>
 					<div className="learn-more">
 						<a className="button" href="/">
@@ -71,8 +72,8 @@ function Details() {
 				</div>
 				<div className="pic">
 					<img
-						alt="beginnings"
-						src="https://uploads-ssl.webflow.com/62be0f5753ff54221209cbb4/62be0f5853ff5416b009cbef_nathan-fertig-249917-unsplash.jpg"
+						alt="products"
+						src="https://uploads-ssl.webflow.com/62be0f5753ff54221209cbb4/62be0f5853ff54282e09cbf0_julian-hochgesang-795082-unsplash.jpg"
 					/>
 				</div>
 			</div>
