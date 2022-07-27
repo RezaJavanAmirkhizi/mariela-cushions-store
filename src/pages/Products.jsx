8490d73de4ae64fed4011a94e6247890fe8d14ff
Reza Javan <javan.reza1380@gmail.com>
2022-07-27 11:51:52 +0430

    Connect these pages to store and get data from api

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,53 +1,49 @@
 import Product from "../components/Product";
+import { connect } from "react-redux";
+import getAllProducts from "../data/action/getAllProducts";
+import { useState, useEffect } from "react";
 
-function Products() {
-	const products = [
-		{
-			name: "Yellow Cushion",
-			price: 19.99,
-			image: "https://th.bing.com/th/id/OIP.wz3YBB1Ntvltpn45vPtrFgHaHb?pid=ImgDet&rs=1",
-		},
-		{
-			name: "Silver Cushion",
-			price: 19.99,
-			image: "https://th.bing.com/th/id/OIP.Xp3zWW8FXmGgin-7hoIQcwHaHa?pid=ImgDet&rs=1",
-		},
-		{
-			name: "Deep Blue Cushion",
-			price: 19.99,
-			image: "https://th.bing.com/th/id/OIP.6m5kTr77ai4kJYz4M7rGgQHaHa?pid=ImgDet&w=600&h=600&rs=1",
-		},
-		{
-			name: "Pink Cushion",
-			price: 19.99,
-			image: "https://th.bing.com/th/id/OIP._iaXZW-LAyVutANWxtZzowHaHa?pid=ImgDet&w=1000&h=1000&rs=1",
-		},
-		{
-			name: "Black Cushion",
-			price: 19.99,
-			image: "https://images-na.ssl-images-amazon.com/images/I/51fJqVavmIL._AC_SX679_.jpg",
-		},
-		{
-			name: "Green Cushion",
-			price: 19.99,
-			image: "https://th.bing.com/th/id/OIP.rh7gnPPn9weu7YKyVvvu6wHaHa?pid=ImgDet&rs=1",
-		},
-	];
+function Products(props) {
+	//Create this state to set products from api
+	const [products, setProducts] = useState([]);
+
+	useEffect(() => {
+		props.getAllProducts();
+		setProducts(props.products);
+	}, []);
+
+	useEffect(() => {
+		setProducts(props.products);
+	}, [props.products]);
 
 	return (
-		<div className="products">
-			{products.map((item, index) => {
-				return (
-					<Product
-						key={index}
-						image={item.image}
-						name={item.name}
-						price={item.price}
-					/>
-				);
-			})}
-		</div>
+		<>
+			<div className="page-title">
+				<h1>Products</h1>
+			</div>
+			<div className="products">
+				{products.length !== 0
+					? products.map((item) => {
+							return (
+								<Product
+									key={item.id}
+									id={item.id}
+									image={item.image}
+									name={item.name}
+									price={item.price}
+								/>
+							);
+					  })
+					: ""}
+			</div>
+		</>
 	);
 }
 
-export default Products;
+const mapStateToProps = (state) => {
+	return {
+		products: state.products,
+	};
+};
+
+export default connect(mapStateToProps, { getAllProducts })(Products);
