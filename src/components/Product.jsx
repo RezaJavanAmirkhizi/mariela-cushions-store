b38ea61ae0fad76e1fe730af7cb928e68a60695b
Reza Javan <javan.reza1380@gmail.com>
2022-07-27 11:52:36 +0430

    Connect this page to store and fetch data when page was reload

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,13 +1,27 @@
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import getProductsByFilter from "../data/action/getProductsByFilter";
+
 function Product(props) {
 	return (
-		<a className="product" href="/">
+		<Link
+			onClick={() => props.getProductsByFilter("id", props.id)}
+			className="product"
+			to={`/product_detail/${props.id}`}
+		>
 			<div className="pic">
 				<img src={props.image} alt={props.name} />
 			</div>
 			<h3>{props.name}</h3>
 			<p>{`$${props.price} USD`}</p>
-		</a>
+		</Link>
 	);
 }
 
-export default Product;
+const mapStateToProps = (state) => {
+	return {
+		products: state.products,
+	};
+};
+
+export default connect(mapStateToProps, { getProductsByFilter })(Product);
