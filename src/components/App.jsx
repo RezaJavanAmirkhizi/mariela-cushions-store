bfdc35986417d0cf9ec8f1ec8fb29bf3b94a5f00
Reza Javan <javan.reza1380@gmail.com>
2022-07-27 11:52:52 +0430

    Route some pages

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,6 +4,8 @@ import NavBar from "./navBar/NavBar";
 import Home from "../pages/Home";
 import Footer from "./footer/Footer";
 import Products from "../pages/Products";
+import ProductDetail from "../pages/ProductDetail";
+import CheckOut from "../pages/CheckOut";
 
 function App() {
 	return (
@@ -12,6 +14,8 @@ function App() {
 			<Routes>
 				<Route exact path="/" element={<Home />} />
 				<Route exact path="/products" element={<Products />} />
+				<Route path="/product_detail/:id" element={<ProductDetail />} />
+				<Route path="/checkout" element={<CheckOut />} />
 			</Routes>
 			<Footer />
 		</div>
