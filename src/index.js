b1de4afd6fcac91e20928e8cddb056546660321f
Reza Javan <javan.reza1380@gmail.com>
2022-07-31 12:51:02 +0430

    Add scroll to top for all routes

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import App from "./components/App";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./data/store";
+import ScrollToTop from './components/ScrollToTop';
 
 ReactDOM.render(
 	<React.StrictMode>
 		<BrowserRouter>
 			<Provider store={store}>
+				<ScrollToTop />
 				<App />
 			</Provider>
 		</BrowserRouter>
