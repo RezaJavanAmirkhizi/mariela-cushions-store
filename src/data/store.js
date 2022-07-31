e8c8fcfed7314401d960fe84be6bf0ee25f8b84f
Reza Javan <javan.reza1380@gmail.com>
2022-07-31 12:53:45 +0430

    Add user reducers to store

--- src/data/store.js
+++ src/data/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
-import productsReducer from "./reducer/productsReducer";
+import productsReducer from "./reducers/productsReducer";
+import usersReducer from "./reducers/userReducer";
 
 const store = configureStore({
 	reducer: {
 		products: productsReducer,
+		users: usersReducer
 	},
 });
 
